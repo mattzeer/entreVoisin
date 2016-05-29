8c09bfcc13f7d9931013ea388dea4a8689208de3
Callixte <Anthony-bailly@live.fr>
2016-05-29 16:45:46 +0200

    Envent + Covoiturage + Objet Ajout

--- EntreVoisin/Controllers/ActiviteController.cs
+++ EntreVoisin/Controllers/ActiviteController.cs
@@ -114,6 +114,84 @@ namespace EntreVoisin.Controllers
                 return View();
             }
         }
+        [HttpPost]
+        public ActionResult AddObjet(ObjetActiviteModelView model)
+        {
+            UTILISATEUR u = db.UTILISATEUR.Where(m => m.IDUSER.Equals(model.idUser)).FirstOrDefault();
+            if (ModelState.IsValid)
+            {
+                ACTIVITE a = new ACTIVITE();
+                a.IDUSER = u.IDUSER;
+                a.IDCOMMUNAUTE = u.COMMUNAUTE.FirstOrDefault().IDCOMMUNAUTE;
+                a.DESCRIP = model.message;
+                a.TYPEACTIVITE = "OBJET";
+                a.DATECREATION = DateTime.Now;
+                a.OBJET = new OBJET();
+                //a.OBJET.CDOBJET = model.cdObjet;
+                //a.OBJET.CDPROPOSITIONOBJET = model.cdPropositionObjet;
+                a.OBJET.TITRE = model.titreObjet;
+                a.OBJET.PRIX = model.prixObjet;
+                db.ACTIVITE.Add(a);
+                db.SaveChanges();
+                return RedirectToAction("Index", "User");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpPost]
+        public ActionResult AddCovoiturage(CovoiturageActiviteModelView model)
+        {
+            UTILISATEUR u = db.UTILISATEUR.Where(m => m.IDUSER.Equals(model.idUser)).FirstOrDefault();
+            if (ModelState.IsValid)
+            {
+                ACTIVITE a = new ACTIVITE();
+                a.IDUSER = u.IDUSER;
+                a.IDCOMMUNAUTE = u.COMMUNAUTE.FirstOrDefault().IDCOMMUNAUTE;
+                a.DESCRIP = model.message;
+                a.TYPEACTIVITE = "COVOIT";
+                a.DATECREATION = DateTime.Now;
+                a.COVOITURAGE = new COVOITURAGE();
+                a.COVOITURAGE.DATEDEPART = model.dateDep;
+                a.COVOITURAGE.DATEARRIVE = model.dateArriv;
+                a.COVOITURAGE.LIEUARRIVE = model.lieuArriv;
+                a.COVOITURAGE.LIEUDEPART = model.lieuDep;
+                a.COVOITURAGE.PARTICIPATION = model.participation;
+                a.COVOITURAGE.VEHICULE = model.vehicule;
+                db.ACTIVITE.Add(a);
+                db.SaveChanges();
+                return RedirectToAction("Index", "User");
+            }
+            else
+            {
+                return RedirectToAction("Index", "User",model);
+            }
+        }
+        public ActionResult AddEvenement(EvenementActiviteModelView model)
+        {
+            UTILISATEUR u = db.UTILISATEUR.Where(m => m.IDUSER.Equals(model.idUser)).FirstOrDefault();
+            if (ModelState.IsValid)
+            {
+                ACTIVITE a = new ACTIVITE();
+                a.IDUSER = u.IDUSER;
+                a.IDCOMMUNAUTE = u.COMMUNAUTE.FirstOrDefault().IDCOMMUNAUTE;
+                a.DESCRIP = model.message;
+                a.TYPEACTIVITE = "EVENT";
+                a.DATECREATION = DateTime.Now;
+                a.EVENEMENT = new EVENEMENT();
+                a.EVENEMENT.DATEEVENT = model.dateEve;
+                a.EVENEMENT.TITRE = model.titreEvenement;
+                a.EVENEMENT.LIEU = model.lieuEvenement;
 
+                db.ACTIVITE.Add(a);
+                db.SaveChanges();
+                return RedirectToAction("Index", "User");
+            }
+            else
+            {
+                return RedirectToAction("Index", "User", model);
+            }
+        }
     }
 }
